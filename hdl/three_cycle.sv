bfafbc9ecbdbdc9b30ca39ba788591e02dd6e4f5
duoduo <jgzhang1998@gmail.com>
2026-02-19 20:43:43 +0100

    3 cycles

--- hdl/three_cycle.sv
+++ hdl/three_cycle.sv
@@ -8,8 +8,8 @@ module three_cycle (
     output logic [15:0] result
 );
     logic [7:0]  a_q, b_q;
-    logic [15:0] mult1, mult2;
-    logic        done1, done2, done3, done_q;
+    logic [15:0] mult1;
+    logic        done1, done2, done3;
     logic        start_r; 
 
     // 检测 start 上升沿，生成单周期脉冲
@@ -25,28 +25,23 @@ module three_cycle (
             a_q     <= '0;
             b_q     <= '0;
             mult1   <= '0;
-            mult2   <= '0;
             result  <= '0;
             done1   <= '0;
             done2   <= '0;
             done3   <= '0;
-            done_q  <= '0;
         end
         else begin
             a_q   <= A;
             b_q   <= B;
             mult1 <= a_q * b_q;
-            mult2 <= mult1;
-            result <= mult2;
+            result <= mult1;
 
-            // 用 start_pulse 替代 start && !done_q，只在上升沿触发一次
+            // 用 start_pulse 只在上升沿触发一次
             done1  <= start_pulse;
-            done2  <= done1  && !done_q;
-            done3  <= done2  && !done_q;
-            done_q <= done3  && !done_q;
+            done2  <= done1;
+            done3  <= done2;
         end
     end
 
-    assign done = done_q;
+    assign done = done3;
 endmodule : three_cycle
-
